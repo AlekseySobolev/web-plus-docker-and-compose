7fd0974994b8d6b3872d8f8f67e947233809d817
AlekseySobolev <sobzone@yandex.ru>
2023-02-21 17:42:46 +0300

    fix docker frontend

--- frontend/nginx/conf.d
+++ frontend/nginx/conf.d
@@ -1,11 +1,23 @@
 server {
-    listen       80;
-    server_name  alexsobolev.nomoredomains.work;
+    lserver {
+        
+        listen 80;
 
-    location / {
-        root   /usr/share/nginx/html;
-        index  index.html index.htm;
-                # Исправляем роутинг на фронтенде
-        try_files $uri $uri/ /index.html;
+        server_name alexsobolev.nomoredomains.work;
+
+        location / {
+
+
+                proxy_pass http://alexsobolev.nomoredomains.work:8081;
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection 'upgrade';
+                proxy_set_header Host $host;
+                proxy_hide_header Access-Control-Allow-Origin;
+                add_header Access-Control-Allow-Origin "$http_origin" always;
+                proxy_cache_bypass $http_upgrade;
+        }
+
     }
+
 } 
